cb5d0da7e6eac84851fc7b0a6b4d9b3a521d1626
Ansh Gupta <anshgupta651@gmail.com>
2025-08-07 11:06:00 +0530

    mail changes 1

--- backend/utils/sendTicketIdEmail.js
+++ backend/utils/sendTicketIdEmail.js
@@ -1,10 +1,7 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
-// This file now acts as a proxy to the main mail utility for consistency.
-// All email templates are managed in 'mail.js'.
 export { sendTicketIdEmail } from './mail.js';
-// … your existing sendOtpEmail and sendRegistrationEmail …
 
 /**
  * Send the user their grievance ticket ID and resolution timeline.
